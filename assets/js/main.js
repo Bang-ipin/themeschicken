cf07215a24d0386652131bfad33f83f2c6cd4909
Bang-ipin <you@example.com>
2022-05-23 12:47:19 +0700

    update baru

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,9 +18,6 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
     const on = (type, el, listener, all = false) => {
         let selectEl = select(el, all)
         if (selectEl) {
@@ -32,16 +29,10 @@
         }
     }
 
-    /**
-     * Easy on scroll event listener 
-     */
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
 
-    /**
-     * Navbar links active state on scroll
-     */
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
         let position = window.scrollY + 200
@@ -59,9 +50,6 @@
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
 
-    /**
-     * Scrolls to an element with header offset
-     */
     const scrollto = (el) => {
         let header = select('#header')
         let offset = header.offsetHeight
@@ -77,9 +65,6 @@
         })
     }
 
-    /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
     let selectHeader = select('#header')
     if (selectHeader) {
         const headerScrolled = () => {
@@ -93,9 +78,6 @@
         onscroll(document, headerScrolled)
     }
 
-    /**
-     * Back to top button
-     */
     let backtotop = select('.back-to-top')
     if (backtotop) {
         const toggleBacktotop = () => {
@@ -109,18 +91,12 @@
         onscroll(document, toggleBacktotop)
     }
 
-    /**
-     * Mobile nav toggle
-     */
     on('click', '.mobile-nav-toggle', function(e) {
         select('#navbar').classList.toggle('navbar-mobile')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
     })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
     on('click', '.navbar .dropdown > a', function(e) {
         if (select('#navbar').classList.contains('navbar-mobile')) {
             e.preventDefault()
@@ -128,9 +104,6 @@
         }
     }, true)
 
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
     on('click', '.scrollto', function(e) {
         if (select(this.hash)) {
             e.preventDefault()
@@ -146,116 +119,12 @@
         }
     }, true)
 
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
     window.addEventListener('load', () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
                 scrollto(window.location.hash)
             }
         }
-    });
-
-
-    /**
-     *Galery Slider
-     */
-    new Swiper('.galery-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 40
-            },
-            480: {
-                slidesPerView: 2,
-                spaceBetween: 60
-            },
-            640: {
-                slidesPerView: 3,
-                spaceBetween: 80
-            },
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 80
-            }
-        }
-    });
-
-
-    /**
-     * produk Slider
-     */
-    new Swiper('.produk-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 40
-            },
-            480: {
-                slidesPerView: 2,
-                spaceBetween: 60
-            },
-            640: {
-                slidesPerView: 3,
-                spaceBetween: 80
-            },
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 70
-            }
-        }
-    });
-
-    /**
-     * Hero carousel indicators
-     */
-    let heroCarouselIndicators = select("#hero-carousel-indicators")
-    let heroCarouselItems = select('#heroCarousel .carousel-item', true)
-
-    heroCarouselItems.forEach((item, index) => {
-        (index === 0) ?
-        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-            heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-    });
-
-
-
-
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-        AOS.init({
-            duration: 1000,
-            easing: 'ease-in-out',
-            once: true,
-            mirror: false
-        })
     });
 
 })()
