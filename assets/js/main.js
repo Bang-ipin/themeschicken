4eeab1fa1420cd5e55559c730d9ce1b848b4e077
Bang-ipin <you@example.com>
2022-05-24 10:34:30 +0700

    update data

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,4 +127,16 @@
         }
     });
 
+    const menu = document.querySelector(".nav");
+    menu.addEventListener('click', function(e) {
+        const targetMenu = e.target;
+        if(targetMenu.classList.contains('nav-link')) {
+            const menuLinkActive = document.querySelector("ul li a.active");
+            if(menuLinkActive !== null && targetMenu.getAttribute('href') !== menuLinkActive.getAttribute('href')) {
+                menuLinkActive.classList.remove('active');
+            }
+            targetMenu.classList.add('active');
+        }
+    });
+
 })()
